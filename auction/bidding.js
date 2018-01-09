9bb6d917203f06646eef80c52029d6d098076e38
Arthur Quemard <arthur.quemard@viacesi.fr>
2018-01-09 09:29:16 +0100

    Implement dynamic auction closing time process

--- auction/bidding.js
+++ auction/bidding.js
@@ -3,5 +3,6 @@
 exports.bid = function(db, auction_id, user_id, new_price) {
     db.updateBidHistory(auction_id, user_id)
     db.bid(auction_id, user_id, new_price)
+    db.updateEndAuctionDate(auction_id)
     return true
 }
